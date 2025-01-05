a1db27f900e28056fc6f6f953a24634d3257e710
teamcity10101 <teamcity10101@gmail.com>
2025-01-05 18:44:28 +0530

    Update defaulter.py

--- defaulter.py
+++ defaulter.py
@@ -16,7 +16,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = "8146887226:AAH6y2tDhsRKAQLdUvkrqzbIo91xr4wq7UE"
+TOKEN = "8146887226:AAHoOxk1jwSTTwG_3B48_PwkXvto-VKSd5c"
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
 FORWARD_CHANNEL_ID = -1002275420758
 CHANNEL_ID = -1002275420758
